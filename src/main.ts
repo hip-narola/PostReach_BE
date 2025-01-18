60a956fc7c44972a81d1e76817d00c78ec59f4d8
Himani <hip@narola.email>
2025-01-18 20:08:24 +0530

    changes

--- src/main.ts
+++ src/main.ts
@@ -31,7 +31,7 @@ async function bootstrap() {
 			resave: false,
 			saveUninitialized: false,	
 			cookie: {
-				domain: 'postreachbe-production.up.railway.app',
+				domain: 'post-reach-fe.vercel.app',
 				secure: true,//isProduction, // HTTPS in production
 				httpOnly: true, // Prevent client-side JavaScript access
 				sameSite: 'none', // isProduction ? 'none' : 'lax', // 'None' for cross-origin
@@ -48,7 +48,7 @@ async function bootstrap() {
 	app.use(cookieParser());
 	// CORS configuration
 	const allowedOrigins = [
-		configService.get('APP_URL_FRONTEND'),
+		'https://post-reach-fe.vercel.app',
 		'https://postreachbe-production.up.railway.app',
 		'http://localhost:3001'
 	];
