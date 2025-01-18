f0497830f46bbe023b7a3247fa56cf9dc82c30ec
Himani <hip@narola.email>
2025-01-18 17:55:03 +0530

    changes for cookie same site

--- src/main.ts
+++ src/main.ts
@@ -32,9 +32,9 @@ async function bootstrap() {
 			saveUninitialized: false,
 			cookie: {
 				domain: configService.get('COOKIE_DOMAIN'),
-				secure: isProduction, // HTTPS in production
+				secure: true,//isProduction, // HTTPS in production
 				httpOnly: true, // Prevent client-side JavaScript access
-				sameSite: isProduction ? 'none' : 'lax', // 'None' for cross-origin
+				sameSite: 'none', // isProduction ? 'none' : 'lax', // 'None' for cross-origin
 				maxAge: 24 * 60 * 60 * 1000, // 1 day in milliseconds
 			},
 		}),
