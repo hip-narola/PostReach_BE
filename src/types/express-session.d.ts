ec057818561455c666b092a44afd435ff9b934e0
Himani <hip@narola.email>
2025-01-25 12:06:46 +0530

    changes

--- src/types/express-session.d.ts
+++
@@ -1,9 +0,0 @@
-import { Session } from 'express-session';
-
-declare module 'express-session' {
-  interface Session {
-    userId: string; 
-    platformName?: string;  // Add platformName here
-
-  }
-}
