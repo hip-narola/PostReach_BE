1dec5fd90662f198b2593e6574a40b7872da1561
Himani <hip@narola.email>
2025-02-17 14:20:48 +0530

    changes

--- src/shared/common/guards/google/google.guard.ts
+++ src/shared/common/guards/google/google.guard.ts
@@ -1,12 +1,24 @@
 import { ExecutionContext, Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
-
 @Injectable()
 export class GoogleGuard extends AuthGuard('google') {
   async canActivate(context: ExecutionContext) {
     const request = context.switchToHttp().getRequest();
-    request.query = { ...request.query, prompt: 'select_account' }; // Ensures Google shows the account selection screen
+
+    // Ensure the `prompt` parameter is set correctly
+    request.query = { ...request.query,
+                        prompt: 'select_account',
+                        auth_type: 'reauthenticate'
+                    };
+
     return (await super.canActivate(context)) as boolean;
+  }
+
+  handleRequest(err, user, info, context) {
+    if (err || !user) {
+      throw err || new Error('User not authenticated');
+    }
+    return user;
   }
 }
